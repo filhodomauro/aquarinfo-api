c3beae6f5e96c642074fd5f918fa3f9bad5d01e3
Mauro Filho <mauro.filho@gmail.com>
2017-07-08 19:32:21 -0300

    change to java

---
+++ src/main/java/aquar/info/aquarinfoapi/AquarinfoApiApplication.java
@@ -0,0 +1,12 @@
+package aquar.info.aquarinfoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquarinfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquarinfoApiApplication.class, args);
+	}
+}
