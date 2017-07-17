fef83d3d503148ac142e17aeb63a29a36241df2e
Mauro Filho <mauro.filho@gmail.com>
2017-07-17 07:34:45 -0300

    create business

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-  router := gin.Default()
-  router.GET("/health", func(c *gin.Context) {
-    c.String(200,"fuck yeah!")
-  })
-  router.Run()
+	mapping()
+}
+
+func mapping() {
+	router := gin.Default()
+	router.GET("/health", func(c *gin.Context) {
+		c.String(200, "fuck yeah!")
+	})
+	router.Run()
 }
