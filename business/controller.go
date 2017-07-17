0bbe4765a342d01b6a0ac17e4a54110879cc5b27
Mauro Sergio Correa Filho <mauro.correa@kpl.com.br>
2017-07-17 13:58:26 -0300

    business controller

--- business/controller.go
+++ business/controller.go
@@ -1,5 +1,20 @@
 package business
 
-func Search() {
+import "github.com/gin-gonic/gin"
+import "net/http"
 
+type business struct {
+	City string `json:"city"`
+	Name string `json:"name"`
+}
+
+func List(c *gin.Context) {
+	business1 := business{}
+	business1.City = "Barueri - SP"
+	business1.Name = "Petz"
+
+	business2 := business{}
+	business2.City = "Barueri - SP"
+	business2.Name = "Pet Love"
+	c.JSON(http.StatusOK, []business{business1, business2})
 }
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/filhodomauro/aquarinfo-api/business"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	mapping()
@@ -11,5 +14,6 @@ func mapping() {
 	router.GET("/health", func(c *gin.Context) {
 		c.String(200, "fuck yeah!")
 	})
+	router.GET("business", business.List)
 	router.Run()
 }
